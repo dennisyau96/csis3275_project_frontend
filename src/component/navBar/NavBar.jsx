2219e964471080c5bf6d8aae16fd5529aedaeb94
Dennis Yau <>
2024-07-04 22:44:34 -0700

    regular

--- src/component/navBar/NavBar.jsx
+++ src/component/navBar/NavBar.jsx
@@ -2,17 +2,29 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <div className="">
-        <NavLink className="border-2 px-5 py-1" to="/">
+      <div className="flex inset-x-0 border-b-2 justify-end ">
+        <NavLink
+          className="border-2 px-3 py-1 text-black hover:bg-orange-400 hover:text-white active:bg-orange-400 active:text-white"
+          to="/"
+        >
           Home
         </NavLink>
-        <NavLink className="border-2 px-5 py-1" to="/notification">
+        <NavLink
+          className="border-2 px-3 py-1 text-black  hover:bg-orange-400 hover:text-white active:bg-orange-400 active:text-white"
+          to="/notification"
+        >
           Notification
         </NavLink>
-        <NavLink className="border-2 px-5 py-1" to="/booking">
+        <NavLink
+          className="border-2 px-3 py-1 text-black  hover:bg-orange-400 hover:text-white active:bg-orange-400 active:text-white"
+          to="/booking"
+        >
           Manage Booking
         </NavLink>
-        <NavLink className="border-2 px-5 py-1" to="/profile">
+        <NavLink
+          className="border-2 px-3 py-1 text-black  hover:bg-orange-400 hover:text-white active:bg-orange-400 active:text-white"
+          to="/profile"
+        >
           Profile
         </NavLink>
       </div>
