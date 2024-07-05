64ab713e078c2b183fe44125c52a28139f4cb354
Dennis Yau <>
2024-07-05 11:17:41 -0700

    sync

--- src/Views/owner/Owner.jsx
+++ src/Views/owner/Owner.jsx
@@ -2,6 +2,16 @@ function Owner() {
   return (
     <>
       <h1>This is Owner page.</h1>
+      <div className="flex flex-col m-3">
+        <label>User Name</label>
+        <input type="text"></input>
+        <label>Introduction</label>
+        <input type="textarea"></input>
+        <label>Phone Number</label>
+        <input type="tel"></input>
+        <label>Email </label>
+        <input type="email"></input>
+      </div>
     </>
   );
 }
