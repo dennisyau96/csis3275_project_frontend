8b5fdae49424ebd60efd8fcd794bedb48773f50b
Ng Lok Yi <nglokyi0224@gmail.com>
2024-07-05 17:03:58 -0700

    Notifiction Card and BookingRequestCard

---
+++ src/component/Notification/NotificationCard.jsx
@@ -0,0 +1,14 @@
+function NotificationCard() {
+  return (
+    <>
+      <div className="border-2 m-4 w-60 rounded-md border-slate-300 bg-white">
+        <div className="p-2">
+          <div className="text-lg font-bold">Type of notification</div>
+          <div className="text-sm">Details:.....</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NotificationCard;
