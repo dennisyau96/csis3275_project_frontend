9cdc5608115a2837d2addbccd25c749786e4fc28
Ng Lok Yi <nglokyi0224@gmail.com>
2024-07-08 22:29:25 -0700

    Notification popup function and finsihed booking request card

--- src/component/Notification/NotificationCard.jsx
+++ src/component/Notification/NotificationCard.jsx
@@ -1,7 +1,7 @@
 function NotificationCard() {
   return (
     <>
-      <div className="border-2 m-4 w-60 rounded-md border-slate-300 bg-white">
+      <div className="border-2 m-1 w-60 rounded-md border-slate-300 bg-white">
         <div className="p-2">
           <div className="text-lg font-bold">Type of notification</div>
           <div className="text-sm">Details:.....</div>
