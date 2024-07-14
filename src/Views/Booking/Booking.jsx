11914ff7a518f0653c0dbb532a2d96b478b915c1
Ng Lok Yi <nglokyi0224@gmail.com>
2024-07-13 19:56:11 -0700

    ConfirmedBookingCard and LeaveReviewCard

--- src/Views/Booking/Booking.jsx
+++ src/Views/Booking/Booking.jsx
@@ -1,11 +1,16 @@
 import BookingRequestCard from "../../component/ManageBooking/BookingRequestCard";
 import Calendar from "../../component/Calendar/Calendar";
+import ConfirmedBookingCard from "../../component/ManageBooking/ConfirmedBookingCard";
+import LeaveReviewCard from "../../component/Review/LeaveReviewCard";
+
 export default function Booking() {
   return (
     <>
-      <h2>Confirm Booking</h2>
+      <h2>Confirmed Booking</h2>
       <div className="w-full h-auto ">
-        <Calendar />
+        <ConfirmedBookingCard/>
+        <ConfirmedBookingCard/>
+        <ConfirmedBookingCard/>
       </div>
       <h2>Booking Request</h2>
       <div className="w-full block">
