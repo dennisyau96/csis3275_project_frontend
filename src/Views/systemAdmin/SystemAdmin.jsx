4658684bb48ef525ab502dc201a831a2e14e160e
Dennis Yau <>
2024-07-31 16:10:52 -0700

    add footer

--- src/Views/systemAdmin/SystemAdmin.jsx
+++ src/Views/systemAdmin/SystemAdmin.jsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 function SystemAdmin() {
   return (
     <>
-      <div id="adminPage" className="flex flex-col-6 m-0">
+      <div id="adminPage" className="flex flex-col-6 m-3 rounded-sm">
         <div
           id="adminSideBarDiv"
-          className="m-0 bg-orange-100 inline-block w-40 col-span-1"
+          className="m-0 bg-orange-100 inline-block w-40 col-span-1  "
         >
-          <nav id="sidebar" className="m-0">
-            <ul className="list-unstyled">
+          <nav id="sidebar" className="m-0 rounded-s-md">
+            <ul className="list-unstyled rounded-sm">
               <li className="border-b-2 w-full text-center hover:bg-orange-500 hover:text-white p-2 ">
                 <NavLink to="/">Dashboard</NavLink>
               </li>
@@ -46,7 +46,7 @@ function SystemAdmin() {
         {/*page content */}
         <div
           id="pageContent"
-          className="cursor-pointer col col-span-5 bg-white p-5 "
+          className="cursor-pointer col col-span-5 bg-white p-5 rounded-md "
         >
           <div className="">
             <div className="">+ New</div>
